70694509715783cbae254e92ec60685c657a1829
jacqueschen1 <jacqueschen1@gmail.com>
2018-09-09 09:40:55 -0700

    fixes

--- app/src/main/java/com/example/jacqu/vanquish2/Login.java
+++ app/src/main/java/com/example/jacqu/vanquish2/Login.java
@@ -3,6 +3,9 @@ package com.example.jacqu.vanquish2;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -64,6 +67,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
--- app/src/main/java/com/example/jacqu/vanquish2/SecondActivity.java
+++ app/src/main/java/com/example/jacqu/vanquish2/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.jacqu.vanquish2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -11,6 +13,7 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
