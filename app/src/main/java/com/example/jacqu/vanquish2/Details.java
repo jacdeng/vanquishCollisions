24251ec96e1b95434dde3fe8dd265f1d6ab49c21
bca69 <bca69@sfu.ca>
2018-09-08 21:33:21 -0700

    bc

--- app/src/main/java/com/example/jacqu/vanquish2/Details.java
+++ app/src/main/java/com/example/jacqu/vanquish2/Details.java
@@ -1,7 +1,9 @@
 package com.example.jacqu.vanquish2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class Details extends Activity {
 
@@ -11,4 +13,23 @@ public class Details extends Activity {
         setContentView(R.layout.activity_details);
     }
 
+    public void previous1(View view) {
+        Intent previous1 = new Intent(this, Casualty.class);
+
+        startActivity(previous1);
+    }
+
+    public void previous2(View view) {
+        Intent previous2 = new Intent(this, noCollision.class);
+
+        startActivity(previous2);
+    }
+
+    public void lastNext(View view) {
+
+        Intent lastNext = new Intent(this, Thnx_bruh.class);
+
+        startActivity(lastNext);
+
+    }
 }
